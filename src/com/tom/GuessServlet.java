b911d2e37203d76c2dbf7a581e486d1c7c9f475f
Hank Tom <hanktom@gmail.com>
2016-08-09 10:34:33 +0800

    initial commit

---
+++ src/com/tom/GuessServlet.java
@@ -0,0 +1,68 @@
+package com.tom;
+
+import java.io.IOException;
+import java.util.Random;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GuessServlet
+ */
+@WebServlet("/g")
+public class GuessServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final String SECRET = "secret";
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public GuessServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String n = request.getParameter("number");
+		if (n != null) {
+			int number = Integer.parseInt(n);
+			if (request.getSession().getAttribute(SECRET) == null) {// first
+				int secret = new Random().nextInt(10) + 1;
+				request.getSession().setAttribute(SECRET, secret);
+				System.out.println(SECRET + ":" + secret);
+			}
+			String msg = null;
+			int secret = (int) request.getSession().getAttribute(SECRET);
+			if (number > secret) {
+				msg = "小一點吧";
+			} else if (number < secret) {
+				msg = "大一點吧";
+			} else {
+				msg = "猜對了,數字是:"+secret;
+				request.getSession().invalidate();
+			}
+			request.getSession().setAttribute("message", msg);
+			response.sendRedirect("guess.jsp");
+		}
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
